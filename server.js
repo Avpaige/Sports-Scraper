da4b40c623da591346400d03d3e3d6a6c43e686f
Ari Virgil-Paige <avpaige@gmail.com>
2019-10-17 21:22:03 -0700

    handlebars integration/ start of html routes

--- server.js
+++ server.js
@@ -5,29 +5,22 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+const databaseUrl = "scaraperdb";
+const collections = ["articles"];
 
-axios.get("https://www.nba.com/").then(response => {
+const db = require("./models");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-    let $ = cheerio.load(response.data);
-    let results = [];
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
 
-    $("div.content_list--item_wrapper").children().each(function (i, element) {
-
-        let title = $(element).children().text();
-        let link = $(element).attr("href");
-
-
-        results.push({
-            title: title,
-            link: "nba.com"+link
-        });
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main"
     })
-    console.log(results);
-});
+  );
+  app.set("view engine", "handlebars");
 
-
-
-//ESPN
-//div.headlineStack - sort of worked headline but no link
-
-// NOT WORK:  ul.headlineStack_list  div.headlineStack_listContainer section.headlineStack_listContainer ul.headlineStack_list
